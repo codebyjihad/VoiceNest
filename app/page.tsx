1de49993bf9aebf436fbf76d592a54ebd0ef0929
jihad <codebyjihad@gmail.com>
2026-02-26 03:29:10 +0600

    feat: create user dashboard pages

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ import React from 'react'
 const page = () => {
   return (
     <>
-       <Navbar/>
-       <HomeHero/>
-       <AboutProject/>
-       <DahboardImage/>
-       <Footer/>
+      <Navbar />
+      <HomeHero />
+      <DahboardImage />
+      <AboutProject />
+      <Footer />
     </>
   )
 }
