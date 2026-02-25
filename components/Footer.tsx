26542ddb63a4b9ac89be8b64a7c8eb2349c404bd
jihad <codebyjihad@gmail.com>
2026-02-26 03:20:35 +0600

    feat:create footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Footer = () => {
+    const date = new Date()
   return (
-    <div className='p-10 bg-gray-300 dark:bg-black'>
+    <div className='p-5 bg-gray-300 dark:bg-black'>
         <div className='flex flex-col justify-center items-center'>
-             <div>
-                <p>Voice<span>Nest</span></p>
+             <div className='mb-3'>
+                <p className='text-foreground font-bold text-2xl'>Voice<span className='text-primary'>Nest</span></p>
+             </div>
+             <div className='border-t-1 w-full border-secondary text-center py-3'>
+                    <span className='text-gray-600'>© {date.getFullYear()} Voice Nest. All rights reserved.</span>
              </div>
         </div>
     </div>
