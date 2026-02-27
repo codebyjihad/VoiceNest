23f899ff8dd80e7e6d3b7281c4fc0869835e1de4
jihad <codebyjihad@gmail.com>
2026-02-27 11:23:04 +0600

    feat: modify code

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-import Footer from '@/components/landing/Footer'
-import AboutProject from '@/components/landing/AboutProject'
-import DahboardImage from '@/components/landing/DahboardImage'
-import HomeHero from '@/components/landing/HomeHero'
-import Navbar from '@/components/landing/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
-    <>
-      <Navbar />
-      <HomeHero />
-      <DahboardImage />
-      <AboutProject />
-      <Footer />
-    </>
+    <div>page</div>
   )
 }
 
-export default page                                                                                          
+export default page
